b69b2a7e8888490adcde9bf3b367bd7d3b0d0443
ytakky2014 <y.takky2014@gmail.com>
2017-11-19 17:13:28 +0900

    add users-list api

--- api/main.go
+++ api/main.go
@@ -1,7 +1,20 @@
 package main
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
+	"time"
 )
+
+type User struct {
+	gorm.Model
+	Id int `gorm:"primary_key"`
+	Name string
+	Email string
+	CreatedAt time.Time `json:"string"`
+	UpdatedAt time.Time `json:"string"`
+}
 
 func main() {
 	r := gin.Default()
@@ -10,5 +23,20 @@ func main() {
 			"message": "Hello World!!",
 		})
 	})
+
+	r.GET("/users", func(c *gin.Context) {
+		users := getUsers()
+		c.JSON(200, users)
+	})
 	r.Run()
+}
+
+func getUsers() ([]User) {
+	db, _:= gorm.Open("postgres", "host=127.0.0.1 user=user dbname=user sslmode=disable password=password")
+	defer db.Close()
+
+	usersModel := []User{}
+	db.Find(&usersModel)
+
+	return usersModel
 }
